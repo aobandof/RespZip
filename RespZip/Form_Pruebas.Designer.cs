c4d304084420e024dfd6c4c6afe51e4e92385a7b
ofaber82 <ofaber@outlook.com>
2014-10-13 15:19:38 -0300

    2. creacion de primer form pruebas

--- RespZip/Form_Pruebas.Designer.cs
+++ RespZip/Form_Pruebas.Designer.cs
@@ -28,19 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.startAsyncButton = new System.Windows.Forms.Button();
+            this.cancelAsyncButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(44, 22);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 13);
+            this.resultLabel.TabIndex = 0;
+            // 
+            // startAsyncButton
+            // 
+            this.startAsyncButton.Location = new System.Drawing.Point(47, 114);
+            this.startAsyncButton.Name = "startAsyncButton";
+            this.startAsyncButton.Size = new System.Drawing.Size(112, 42);
+            this.startAsyncButton.TabIndex = 1;
+            this.startAsyncButton.Text = "INICIAR";
+            this.startAsyncButton.UseVisualStyleBackColor = true;
+            this.startAsyncButton.Click += new System.EventHandler(this.startAsyncButton_Click);
+            // 
+            // cancelAsyncButton
+            // 
+            this.cancelAsyncButton.Location = new System.Drawing.Point(188, 114);
+            this.cancelAsyncButton.Name = "cancelAsyncButton";
+            this.cancelAsyncButton.Size = new System.Drawing.Size(112, 42);
+            this.cancelAsyncButton.TabIndex = 1;
+            this.cancelAsyncButton.Text = "CANCELAR";
+            this.cancelAsyncButton.UseVisualStyleBackColor = true;
+            this.cancelAsyncButton.Click += new System.EventHandler(this.cancelAsyncButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
             // 
             // Form_Pruebas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 344);
+            this.ClientSize = new System.Drawing.Size(342, 198);
+            this.Controls.Add(this.cancelAsyncButton);
+            this.Controls.Add(this.startAsyncButton);
+            this.Controls.Add(this.resultLabel);
             this.Name = "Form_Pruebas";
             this.Text = "Form_Pruebas";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button startAsyncButton;
+        private System.Windows.Forms.Button cancelAsyncButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- RespZip/Form_Pruebas.cs
+++ RespZip/Form_Pruebas.cs
@@ -15,6 +15,65 @@ namespace RespZip
         public Form_Pruebas()
         {
             InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+        private void startAsyncButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy!=true)
+            {
+                //iniciamos la operacion asincrona o en segundo plano
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void cancelAsyncButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation==true)
+            {
+                //cancelamos la operacion asincrona
+                backgroundWorker1.CancelAsync();
+            }
+        }
+        //el siguiente Controlador de evento es donde se  hace el trabajo con mucho tiempo consumido
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            for (int i = 1; i <= 10; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    //realiza la operacion que consume tiempo y reporta el progreso
+                    System.Threading.Thread.Sleep(500);
+                    worker.ReportProgress(i * 10);
+                }
+            }
+        }
+        //Este controlador de Evento actualiza el progreso
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            resultLabel.Text = (e.ProgressPercentage.ToString() + " %");
+        }
+        //Este controlador de Eventos se refiere a los resultados de la operecion asincrona
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                resultLabel.Text = "Cancelado!";
+            }
+            if (e.Error != null)
+            {
+                resultLabel.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                resultLabel.Text = "Esta Hecho!";
+            }
         }
     }
 }
